22ee7ff1d8d45900b72aaf3eecd0e54aef855270
Berkan Diler <thetoerk@gmail.com>
2016-01-11 20:24:39 +0100

    .

--- app/assets/javascript/battles/commandhandling.js
+++ app/assets/javascript/battles/commandhandling.js
@@ -631,7 +631,6 @@ battledata.dealWithMovemessage = function(params) {
     if (mess.contains("%a")) mess = mess.replace("%a", abilityinfo.name(params.other));
     if (mess.contains("%p")) mess = mess.replace("%p", pokeinfo.name(params.other));
     if (mess.contains("%e")) mess = mess.replace("%e", poke.name);
-)
     
     this.print("<span class='battle-message-" + typeinfo.name(params.type).toLowerCase() + "'>" + mess + "</span>");
 };
