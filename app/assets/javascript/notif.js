cc250649663cea86e2f9cd022c702c4b71245129
Heark <antwauntune25@wcpsstudent.org>
2016-05-13 14:48:54 -0400

    remove Object.watch, Animation only on initial notification

--- app/assets/javascript/notif.js
+++ app/assets/javascript/notif.js
@@ -10,67 +10,47 @@ notif.dec() - Removes 1 notification
 notif.forEach(array) - Adds 1 notification for each value in array
 
 */
+var notif = {
+    favicon: new Favico({
+        animation: "pop",
+        type: "circle"
+    }),
+    faviconNoAnim: new Favico({
+        animation: "none",
+        type: "circle"
+    }),
+    count: 0,
+    one_count: 0, // inital notification
 
-Object.prototype.watch || Object.defineProperty(Object.prototype, "watch", {
-    enumerable: !1,
-    configurable: !0,
-    writable: !1,
-    value: function(t, n) {
-        var e = this[t],
-            o = e,
-            i = function() {
-                return o
-            },
-            c = function(i) {
-                return e = o, o = n.call(this, t, e, i)
-            };
-        delete this[t] && Object.defineProperty(this, t, {
-            get: i,
-            set: c,
-            enumerable: !0,
-            configurable: !0
-        })
-    }
-}), Object.prototype.unwatch || Object.defineProperty(Object.prototype, "unwatch", {
-    enumerable: !1,
-    configurable: !0,
-    writable: !1,
-    value: function(t) {
-        var n = this[t];
-        delete this[t], this[t] = n
-    }
-});
-var o = {
-        p: 0
+    update: function() {
+        notif.count++
     },
-    notif = {
-        favicon: new Favico({
-            animation: "pop",
-            type: "circle"
-        }),
-        faviconNoAnim: new Favico({
-            animation: "none",
-            type: "circle"
-        }),
-        count: 0,
-        update: function() {
-            notif.count++, o.p++
-        },
-        titleUpdate: function() {
-            o.watch("p", function(t, n, e) {
-                return notif.favicon.badge(notif.count), e
-            }), 0 == notif.count ? notif.favicon.badge("") : notif.faviconNoAnim.badge(notif.count)
-        },
-        clear: function() {
-            notif.count = 0
-        },
-        dec: function() {
-            notif.count--
-        },
-        forEach: function(t) {
-            for (var n = 0; n < t.length; n++) notif.update()
+
+    titleUpdate: function() {
+        if (notif.count == 1) {
+            if (notif.one_count == 1) { // checks for initial notification to prevent looping animation
+                notif.faviconNoAnim.badge(notif.count)
+            } else {
+                notif.one_count = 1
+                notif.favicon.badge(notif.count)
+            }
+        } else if (notif.count == 0) {
+            notif.faviconNoAnim.badge("");
+        } else {
+            notif.faviconNoAnim.badge(notif.count)
         }
-    };
+
+    },
+    clear: function() {
+        notif.count = 0
+    },
+    dec: function() {
+        notif.count--
+    },
+    forEach: function(t) {
+        for (var n = 0; n < t.length; n++) notif.update()
+    }
+};
 setInterval(function() {
     notif.titleUpdate()
 }, 1e3);
